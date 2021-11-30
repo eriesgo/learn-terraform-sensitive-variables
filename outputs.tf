b1c088d78005dfd24f74c1daf533e65dae96599e
Enrique Riesgo-Canal <eriesgocanal@dxc.com>
2021-11-30 12:23:59 +0100

    Add sensitive output

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,8 @@
 # Output declarations
+
+output "db_connect_string" {
+  description = "MySQL database connection string"
+  value       = "Server=${aws_db_instance.database.address}; Database=ExampleDB; Uid=${var.db_username}; Pwd=${var.db_password}"
+  # Marks the variables as sensitive (TF14+ only)  
+  #sensitive   = true
+}
